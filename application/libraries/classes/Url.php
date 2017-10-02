5a1f083c6ddc9c9029fdb92d65de69f53dceda63
Em1K3l <Em1K3l@Em1K3l-PC>
2017-10-02 21:17:55 +0300

    fixing bug with redirect from non-www to www and vice versa

--- application/libraries/classes/Url.php
+++ application/libraries/classes/Url.php
@@ -79,6 +79,7 @@ class Url {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->urlAddress);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         curl_close($ch);
